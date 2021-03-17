aeac4bab881f67e53910e379dc156f69dd24ec09
Sean F. Cleator <seancleator@hotmail.co.uk>
2021-03-17 00:32:58 +0000

    Added config to wsgi

--- cursas/wsgi.py
+++ cursas/wsgi.py
@@ -2,11 +2,13 @@ import dash
 
 from cursas.visualise import build_full_app 
 
+from cursas.config import get_config
+
 app = dash.Dash(
         title='Cursas',
         url_base_pathname='/cursas/',
         #assets_folder='../assets'
         )
 
-app = build_full_app(app)
+app = build_full_app(app, get_config())
 application = app.server
